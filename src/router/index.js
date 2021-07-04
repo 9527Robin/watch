fc10479ec6d8ccbb99a03f7d61bdaf102b2b00c1
luobin <1023313553@qq.com>
2021-07-04 10:02:08 +0800

    代码更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/home/home.vue";
-import Main from "../views/main/main.vue";
 import Demo from "../views/demo/Demo.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    // // name: "Home",
-    // redirect: "/main"
-    component: Home
-  },
-  {
-    path: "/main",
-    name: "main",
-    component: Main
-  },
-  {
-    path: "/demo",
     name: "demo",
     component: Demo
   }
