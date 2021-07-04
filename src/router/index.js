a48260675d3cec0f43ff30e254e090e862d19ad2
luobin <luobin@cloudstream-tech.com>
2021-07-04 09:49:01 +0800

    Other: watch

--- src/router/index.js
+++ src/router/index.js
@@ -2,26 +2,30 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/home/home.vue";
 import Main from "../views/main/main.vue";
+import Demo from "../views/demo/Demo.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    // name: "Home",
-    redirect: "/main",
-    // component: Home
+    // // name: "Home",
+    // redirect: "/main"
+    component: Home
   },
   {
     path: "/main",
     name: "main",
-    component: Main,
+    component: Main
   },
+  {
+    path: "/demo",
+    name: "demo",
+    component: Demo
+  }
 ];
 
 const router = new VueRouter({
-  // mode: "history",
-  // base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
---
+++ src/views/demo/Demo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="demo">
+    <ul @click="changeFont">
+      <li
+        v-for="(item, index) in numbers"
+        :data-index="index"
+        :key="index"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: 25
+    };
+  },
+  methods: {
+    changeFont(e) {
+      const data = _.get(e, ["target", "innerHTML"], "");
+      console.log(e, data, "data");
+      switch (data) {
+        case "":
+          e.target.innerHTML = "男";
+          break;
+        case "男":
+          e.target.innerHTML = "女";
+          break;
+        case "女":
+          e.target.innerHTML = "";
+          break;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.demo {
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    li {
+      font-size: 16px;
+      height: 50px;
+      width: 20px;
+      border: 1px solid #000;
+      width: 20%;
+      box-sizing: border-box;
+      span {
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
