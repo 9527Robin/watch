bc2d5ce62300d8d38739a7f3aab5387dc53c61fe
luobin <1023313553@qq.com>
2021-07-04 11:30:28 +0800

    界面一输入逻辑基本完成

--- src/views/demo/Demo.vue
+++ src/views/demo/Demo.vue
@@ -5,6 +5,7 @@
         v-for="(item, index) in numbers"
         :data-index="index"
         :key="index"
+        :class="handleStatus(index) ? 'abled-icon' : 'disabled-icon'"
       ></li>
     </ul>
   </div>
@@ -14,23 +15,43 @@
 export default {
   data() {
     return {
-      numbers: 25
+      numbers: 25,
+      view1Data: [""]
     };
   },
   methods: {
     changeFont(e) {
-      const data = _.get(e, ["target", "innerHTML"], "");
-      console.log(e, data, "data");
-      switch (data) {
-        case "":
-          e.target.innerHTML = "男";
-          break;
-        case "男":
-          e.target.innerHTML = "女";
-          break;
-        case "女":
-          e.target.innerHTML = "";
-          break;
+      const classList = e.target.classList;
+      const index = e.target.dataset.index;
+      if (!this.handleStatus(index)) return;
+      classList.remove("abled-icon");
+      if (!classList.contains("boy-icon")) {
+        // 当女 => 男时
+        if (classList.contains("girl-icon")) {
+          classList.remove("girl-icon");
+        }
+        classList.add("boy-icon");
+        this.$set(this.view1Data, index, "BOY");
+        return;
+      } else {
+        classList.remove("boy-icon");
+      }
+      if (!classList.contains("girl-icon")) {
+        classList.add("girl-icon");
+        this.$set(this.view1Data, index, "GIRL");
+        return;
+      } else {
+        classList.remove("girl-icon");
+      }
+    },
+    handleStatus(index) {
+      if (index == "0") {
+        return true;
+      }
+      if (_.get(this.view1Data, index - 1, "")) {
+        return true;
+      } else {
+        return false;
       }
     }
   }
@@ -42,6 +63,8 @@ export default {
   ul {
     display: flex;
     flex-wrap: wrap;
+    height: 250px;
+    flex-direction: column;
     li {
       font-size: 16px;
       height: 50px;
@@ -49,11 +72,24 @@ export default {
       border: 1px solid #000;
       width: 20%;
       box-sizing: border-box;
+
       span {
         height: 100%;
         width: 100%;
       }
     }
+  }
+  .boy-icon {
+    background: url("../../assets/images/boy.png") no-repeat center;
+  }
+  .girl-icon {
+    background: url("../../assets/images/girl.png") no-repeat center;
+  }
+  .disabled-icon {
+    background: #999;
+  }
+  .abled-icon {
+    background: #fff;
   }
 }
 </style>
